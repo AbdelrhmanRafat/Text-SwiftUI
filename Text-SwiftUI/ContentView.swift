1ebe348a9ebfd680240209ec9d0bdd56f76867b1
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2022-10-10 16:06:49 +0200

    Text Font

---
+++ Text-SwiftUI/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Text-SwiftUI
+//
+//  Created by Macbook on 10/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Life is like a marathon not a sprint")
+            .fontWeight(.heavy)
+            .font(.title)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Text-SwiftUI/Text_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text_SwiftUIApp.swift
+//  Text-SwiftUI
+//
+//  Created by Macbook on 10/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Text_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
