3b7a8ffa39e7f5a8e3ec6c9998678d61447e6604
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2022-10-12 09:09:14 +0200

    Change the Font Type and color

--- Text-SwiftUI/ContentView.swift
+++ Text-SwiftUI/ContentView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        VStack (alignment: .center, spacing: 40){
         Text("Stay Hungry Stay Foolish")
+            //it's simple to read
+            .font(.title) // Set Font type
+            .fontWeight(.bold) // Set Font Weight
+            .foregroundColor(.blue) // Set Text Color
+        // there is other way to set text Font Modifier
+           Text("Stay Hungry Stay Foolish")
+            .font(.system(size: 28, weight: .bold, design: .rounded)) // in case you want fixed size font
+            .foregroundColor(.green)
+        }
     }
 }
 
