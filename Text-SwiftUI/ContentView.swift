7256740d1ee3711f27c5b6b3e4bef70b769fb18d
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2023-01-04 11:17:45 +0200

    Working With MultiLine Text.

--- Text-SwiftUI/ContentView.swift
+++ Text-SwiftUI/ContentView.swift
@@ -9,17 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack (alignment: .center, spacing: 40){
-        Text("Stay Hungry Stay Foolish")
-            //it's simple to read
-            .font(.title) // Set Font type
-            .fontWeight(.bold) // Set Font Weight
-            .foregroundColor(.blue) // Set Text Color
-        // there is other way to set text Font Modifier
-           Text("Stay Hungry Stay Foolish")
-            .font(.system(size: 28, weight: .bold, design: .rounded)) // in case you want fixed size font
-            .foregroundColor(.green)
-        }
+        Text("Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma—which is living with the results of other people’s thinking. Don’t let the noise of others’ opinions drown out your own inner voice. And most important, have the courage to follow your heart and intuition.")
+            .fontWeight(.bold)
+            .font(.title)
+            .foregroundColor(.gray)
+            .multilineTextAlignment(.center) // To Align Text in Center
+            .lineLimit(6) // to Limit the Lines : the default value for line limit is nil.
+            .truncationMode(.tail)// Detrmine the truncation of text for line limit
+            .lineSpacing(10)// Controls space between lines in multiline text
+            .padding(.horizontal, 50.0) // Set Padding horizontally For Text
+            .rotation3DEffect(.degrees(30), axis: (x: 1, y: 0, z: 0)) // Rotation Effect
+           .shadow(color: .gray, radius: 2, x: 0, y: 20)
+            // Set Shadow for Text
     }
 }
 
